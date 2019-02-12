93a322e56f5f47cdf5b0dee7889abe3a8022e688
Rogier Verkaik <rogierv@gmail.com>
2019-02-12 12:18:04 +0100

    Add POST and PUT routers

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,4 +1,12 @@
-import { JsonController, Get, Param } from 'routing-controllers';
+import {
+  JsonController,
+  Get,
+  Param,
+  Put,
+  Body,
+  Post,
+  HttpCode
+} from 'routing-controllers';
 import pagesById, { Page, PageList } from './data';
 
 @JsonController()
@@ -11,5 +19,18 @@ export default class PageController {
   @Get('/pages')
   allPages(): PageList {
     return { pages: Object.values(pagesById) };
+  }
+
+  @Put('/pages/:id')
+  updatePage(@Param('id') id: number, @Body() body: Partial<Page>): Page {
+    console.log(`Incoming PUT body params:`, body);
+    return pagesById[id];
+  }
+
+  @Post('/pages')
+  @HttpCode(201)
+  createPage(@Body() body: Page): Page {
+    console.log(`Incoming POST body param:`, body);
+    return body;
   }
 }
